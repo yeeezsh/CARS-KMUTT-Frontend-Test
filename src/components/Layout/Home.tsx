e2ce4e6e726cebbff6f1cad1b5f100128b032c5c
yee2542 <yee2542gtl@gmail.com>
2020-01-05 12:07:30 +0700

    add drawer

--- src/components/Layout/Home.tsx
+++ src/components/Layout/Home.tsx
@@ -1,38 +1,45 @@
-import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import React, { Component, Props } from 'react'
+import { Row, Col, Drawer } from 'antd'
 import styles from './home.module.css'
-import hamburger from '../../assets/icons/hamburger-orange.svg'
+import hamburgerOrange from '../../assets/icons/hamburger-orange.svg'
+import hamburgerWhite from '../../assets/icons/hamburger-white.svg'
 
-export default class Home extends Component {
-    constructor(props: Readonly<{}>) {
-        super(props);
-        this.state = {
-            collapsed: false,
-        };
+export default class Home extends Component<{}, { drawer: boolean }> {
+
+    state: { drawer: boolean } = {
+        drawer: false
     }
 
-    state = {
-        collapsed: true
-    }
-
-    toggle = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
+    onDrawer = () => {
+        this.setState(prevState => {
+            return {
+                drawer: !prevState.drawer
+            }
+        })
     };
 
     render() {
         return (
             <React.Fragment>
+                <Drawer
+                    placement={'left'}
+                    closable={false}
+                    maskClosable={true}
+                    onClose={this.onDrawer}
+                    visible={this.state.drawer}
+                    drawerStyle={{ backgroundColor: '#FF682B' }}
+                >
+                    <img onClick={this.onDrawer} src={hamburgerWhite} alt='hamburger' />
+                </Drawer>
                 <Row className={styles.header}>
                     <Col className={styles.btn} span={1}>
-                        <img src={hamburger} alt='hamburger' />
+                        <img onClick={this.onDrawer} src={hamburgerOrange} alt='hamburger' />
                     </Col>
                     <Col className={styles.title} span={12} offset={5}>
-                        <p className={styles.orange}>hoome</p>
+                        <p className={styles.orange}>หน้าแรก</p>
                     </Col>
                 </Row>
-                <div>sider</div>
+                <div onClick={() => console.log('jaa')} style={{ height: 300, backgroundColor: 'black' }}>sider</div>
                 <Row style={{ marginTop: 50, padding: 0 }}>
                     <div>{this.props.children}</div>
                 </Row>
