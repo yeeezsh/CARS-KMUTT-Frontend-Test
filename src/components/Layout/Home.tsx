d6be51d0214b03b6908f2ea634f6646ce3a02189
yee2542 <yee2542gtl@gmail.com>
2020-01-05 13:30:07 +0700

    add date welcome

--- src/components/Layout/Home.tsx
+++ src/components/Layout/Home.tsx
@@ -1,13 +1,15 @@
 import React, { Component, Props } from 'react'
 import { Row, Col, Drawer } from 'antd'
+import moment from 'moment'
 import styles from './home.module.css'
-import hamburgerOrange from '../../assets/icons/hamburger-orange.svg'
+// import hamburgerOrange from '../../assets/icons/hamburger-orange.svg'
 import hamburgerWhite from '../../assets/icons/hamburger-white.svg'
 
-export default class Home extends Component<{}, { drawer: boolean }> {
+export default class Home extends Component<{}, { drawer: boolean, day: moment.Moment }> {
 
-    state: { drawer: boolean } = {
-        drawer: false
+    state = {
+        drawer: false,
+        day: moment('30/11/20', 'DD-MM-YY')
     }
 
     onDrawer = () => {
@@ -19,6 +21,11 @@ export default class Home extends Component<{}, { drawer: boolean }> {
     };
 
     render() {
+        const { day, drawer } = this.state
+        const dayName = day.format('ddd')
+        const date = day.format('D')
+        const month = day.format('MMMM')
+        const year = day.format('YYYY')
         return (
             <React.Fragment>
                 <Drawer
@@ -26,22 +33,28 @@ export default class Home extends Component<{}, { drawer: boolean }> {
                     closable={false}
                     maskClosable={true}
                     onClose={this.onDrawer}
-                    visible={this.state.drawer}
+                    visible={drawer}
                     drawerStyle={{ backgroundColor: '#FF682B' }}
                 >
                     <img onClick={this.onDrawer} src={hamburgerWhite} alt='hamburger' />
                 </Drawer>
                 <Row className={styles.header}>
                     <Col className={styles.btn} span={1}>
-                        <img onClick={this.onDrawer} src={hamburgerOrange} alt='hamburger' />
+                        <img onClick={this.onDrawer} src={hamburgerWhite} alt='hamburger' />
                     </Col>
                     <Col className={styles.title} span={12} offset={5}>
-                        <p className={styles.orange}>หน้าแรก</p>
+                        <p className={styles.white}>หน้าแรก</p>
                     </Col>
                 </Row>
-                <div onClick={() => console.log('jaa')} style={{ height: 300, backgroundColor: 'black' }}>sider</div>
+
+                {/* date and time */}
+                <Row className={styles.date}>
+                    <div className={styles.textDate}>Today</div>
+                    <div className={styles.textDate}>{dayName}, {date} {month} {year}</div>
+                </Row>
+                {/* content */}
                 <Row style={{ marginTop: 50, padding: 0 }}>
-                    <div>{this.props.children}</div>
+                    <div className={styles.content}>{this.props.children}</div>
                 </Row>
             </React.Fragment>
         )
