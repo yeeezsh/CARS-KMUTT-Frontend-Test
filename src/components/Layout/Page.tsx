ac48c9561a89443a7c18f1e186368325f8d80e6d
yee2542 <yee2542gtl@gmail.com>
2020-01-06 14:01:07 +0700

    fix bad interfaces

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,10 +1,17 @@
-import React, { Component, Props } from 'react'
+import React, { Component, Props as PageProps } from 'react'
 import { Row, Col, Drawer } from 'antd'
 import styles from './page.module.css'
 import hamburgerOrange from '../../assets/icons/hamburger-orange.svg'
-import hamburgerWhite from '../../assets/icons/hamburger-white.svg'
+// import hamburgerWhite from '../../assets/icons/hamburger-white.svg'
+import AppDrawer from '../AppDrawer'
 
-export default class PageLayout extends Component<{ title: string }, { drawer: boolean }> {
+export default class PageLayout extends Component<
+    {
+        titile: string
+    },
+    {
+        drawer: boolean
+    }> {
 
     state = {
         drawer: false
@@ -19,24 +26,16 @@ export default class PageLayout extends Component<{ title: string }, { drawer: b
     };
 
     render() {
+        const { drawer } = this.state
         return (
             <React.Fragment>
-                <Drawer
-                    placement={'left'}
-                    closable={false}
-                    maskClosable={true}
-                    onClose={this.onDrawer}
-                    visible={this.state.drawer}
-                    drawerStyle={{ backgroundColor: '#FF682B' }}
-                >
-                    <img onClick={this.onDrawer} src={hamburgerWhite} alt='hamburger' />
-                </Drawer>
+                <AppDrawer onDrawer={this.onDrawer} drawer={drawer} />
                 <Row className={styles.header}>
                     <Col className={styles.btn} span={1}>
                         <img onClick={this.onDrawer} src={hamburgerOrange} alt='hamburger' />
                     </Col>
                     <Col className={styles.title} span={12} offset={5}>
-                        <p className={styles.orange}>{this.props.title}</p>
+                        <p className={styles.orange}>{this.props.titile}</p>
                     </Col>
                 </Row>
 
