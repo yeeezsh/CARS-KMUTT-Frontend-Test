941824309870357b48ea10f307eacd836d9184b4
yee2542 <yee2542gtl@gmail.com>
2020-01-06 14:02:58 +0700

    refactor homemenu to kanban card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import {} from '../components/Layout/Home'
 import HomeLayout from '../components/Layout/Home'
-import HomeMenu from '../components/HomeMenu'
+import HomeMenu from '../components/KanbanCard'
 
 export default function Home() {
     return (
