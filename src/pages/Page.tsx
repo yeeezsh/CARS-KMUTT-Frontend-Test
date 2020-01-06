6e7bc999dabc950cdd3993e7a0f76eaf07ef21fb
yee2542 <yee2542gtl@gmail.com>
2020-01-06 14:47:17 +0700

    clean code

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PageLayout from '../components/Layout/Page'
 
 export default function Page() {
-    console.log('home')
     return (
         <PageLayout titile='pages'>
             <p>Page jaa</p>
