f00c555054f93abcb052720b433426bf290915f6
yee2542 <yee2542gtl@gmail.com>
2020-01-08 22:08:49 +0700

    add types for components

---
+++ src/pages/Sport/area.interface.ts
@@ -0,0 +1,16 @@
+import { Moment } from 'moment'
+import TimeNode from '../../components/TimeTable/timetable.interface'
+
+interface Area {
+    area: {
+        id: string
+        label: string
+    }
+    time: {
+        start: Moment
+        stop: Moment
+        disabled?: TimeNode[]
+    }
+}
+
+export default Area
