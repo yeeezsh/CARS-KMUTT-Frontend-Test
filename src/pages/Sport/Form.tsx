896cd65f5466e88bdbbd8c821a03be74927809b9
yee2542 <yee2542gtl@gmail.com>
2020-01-09 19:31:02 +0700

    add ids self validator

--- src/pages/Sport/Form.tsx
+++ src/pages/Sport/Form.tsx
@@ -7,6 +7,7 @@ import styles from './styles.module.css'
 import { FormComponentProps } from 'antd/lib/form/Form'
 
 import Outline from '../../components/Outline'
+import Button from '../../components/Button'
 
 class FormPage extends Component<
     RouteComponentProps<any> & FormComponentProps &
@@ -29,6 +30,36 @@ class FormPage extends Component<
         const required = this.props.required || this.state.required
         const users = Array(required).fill('')
         return this.setState({ users })
+    }
+
+    onSubmit = (e: any): void => {
+        e.preventDefault()
+        const { form } = this.props
+        form.validateFields((err, values) => {
+            if (!err) {
+                const { keys, names } = values
+                console.log('Received values of form: ', values)
+                console.log('Merged values:', keys.map((key: any) => names[key]))
+            }
+            console.log(values)
+        })
+    }
+
+    onValidator = (rule: any, value: string, callback: any) => {
+        const { form } = this.props
+        const ids: string[] = form.getFieldValue('users')
+        const sets = new Set(ids).size
+        if (ids.length !== sets)
+            return callback('รหัสนักศึกษาซ้ำ')
+        // if (ids.includes(value)) {
+        //     console.log('summmmm ja')
+        // }
+        console.log('idsss', ids)
+        if (value === '') callback()
+        if (value.length !== 11)
+            return callback('โปรดกรอกรหัสนักศึกษาให้ถูกต้อง')
+        return callback()
+
     }
 
     render() {
@@ -58,27 +89,41 @@ class FormPage extends Component<
                 <Col span={24}>
                     <Row type='flex' justify='center'>
                         <Col span={20}>
-                            <Form>
+                            <Form onSubmit={this.onSubmit}>
                                 {
                                     this.state.users.map((e, i) => {
                                         return (
                                             <Form.Item key={i}>
                                                 {
-                                                    getFieldDecorator('users', {
-                                                        rules: [{ required: true }]
+                                                    getFieldDecorator(`users[${i}]`, {
+                                                        rules: [{
+                                                            required: true,
+                                                            message: 'โปรดกรอกรหัสนักศึกษาให้ถูกต้อง',
+                                                            max: 11
+                                                        },
+                                                        {
+                                                            required: true,
+                                                            validator: this.onValidator,
+                                                        }
+                                                        ],
+                                                        validateTrigger: ['onBlur']
                                                     })(
-                                                        <Input placeholder={`รหัสนักศึกษาคนที่ ${i + 1}`} />
+                                                        <Input
+                                                            placeholder={`รหัสนักศึกษาคนที่ ${i + 1}`}
+                                                            type={'number'}
+                                                        />
                                                     )
                                                 }
                                             </Form.Item>
                                         )
                                     })
                                 }
+                                <Button>ต่อไป</Button>
                             </Form>
                         </Col>
                     </Row>
                 </Col>
-            </React.Fragment>
+            </React.Fragment >
         )
     }
 }
