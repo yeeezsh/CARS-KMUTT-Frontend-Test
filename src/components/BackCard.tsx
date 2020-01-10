2324e37f33ed83277445f212158957e2acfa3c59
yee2542 <yee2542gtl@gmail.com>
2020-01-10 10:36:34 +0700

    add BackCard Components

---
+++ src/components/BackCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const styles: React.CSSProperties = {
+  fontSize: '12px',
+  color: '#FF682B',
+  fontWeight: 'normal',
+  lineHeight: '22px',
+  cursor: 'pointer',
+};
+
+const BackCard: React.FunctionComponent<{
+  onClick?: any;
+  styles?: React.CSSProperties;
+}> = props => {
+  return (
+    <div onClick={() => props.onClick()} style={{ ...styles, ...props.styles }}>
+      {'< '}
+      {props.children}
+    </div>
+  );
+};
+
+export default BackCard;
