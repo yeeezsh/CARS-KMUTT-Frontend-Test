a05c6f30e566eaf6fd3cc450d83c6a5860464913
yee2542 <yee2542gtl@gmail.com>
2020-01-10 16:35:04 +0700

    add more demo sample

--- src/models/sport/index.ts
+++ src/models/sport/index.ts
@@ -49,6 +49,32 @@ const areas: TimeAreaReserveType['areas'] = [
       required: 10,
     },
   },
+  {
+    time: {
+      start: moment().startOf('hour'),
+      stop: moment()
+        .startOf('hour')
+        .add(12, 'hour'),
+      interval: 120,
+      disabled: [
+        {
+          value: moment()
+            .startOf('hour')
+            .add(1, 'hour'),
+        },
+        {
+          value: moment()
+            .startOf('hour')
+            .add(4, 'hour'),
+        },
+      ],
+    },
+    area: {
+      label: 'สนามฟุตบอล 3',
+      id: '2',
+      required: 2,
+    },
+  },
 ];
 
 const stepLists: StepsType[] = [
