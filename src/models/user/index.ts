e3d05f0d1fceca24bee70fd241edb3f26a878c8a
yee2542 <yee2542gtl@gmail.com>
2020-01-15 18:49:11 +0700

    add local storage implements

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -1,6 +1,20 @@
 export default () => null;
 
 import instance from './axios.interface';
+import User from './interface';
+
+const SaveCredential = (data: User) => {
+  try {
+    const duplicated = localStorage.getItem('user');
+    if (duplicated) localStorage.removeItem('user');
+    localStorage.setItem('user', JSON.stringify(data));
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
 const login = (type: 'staff' | 'requestor', data: { username: string; password: string }) => {
   let url = '/users/auth/requestor';
   if (type === 'staff') url = '/users/auth/staff';
@@ -13,6 +27,7 @@ const RequestorLogin = async (
 ): Promise<{ access_token?: string; Authorization?: string; auth: boolean }> => {
   try {
     const res = (await login('requestor', { username, password })).data;
+    SaveCredential(res);
     return { ...res, auth: true };
   } catch (err) {
     const status: number = err.response.status;
