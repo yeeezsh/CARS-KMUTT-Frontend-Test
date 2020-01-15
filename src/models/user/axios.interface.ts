5d50c52d63ebcbf77f37481164806432e48f458f
yee2542 <yee2542gtl@gmail.com>
2020-01-15 18:27:51 +0700

    add error handling

--- src/models/user/axios.interface.ts
+++ src/models/user/axios.interface.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const END_POINT = 'http://localhost:3000';
 const instance = axios.create({
   baseURL: END_POINT,
-  timeout: 10000,
+  timeout: 2000,
 });
 
 export default instance;
--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -10,9 +10,20 @@ const login = (type: 'staff' | 'requestor', data: { username: string; password: 
 const RequestorLogin = async (
   username: string,
   password: string,
-): Promise<{ access_token: string; Authorization: string }> => {
-  const res = (await login('requestor', { username, password })).data;
-  return res;
+): Promise<{ access_token?: string; Authorization?: string; auth: boolean }> => {
+  try {
+    const res = (await login('requestor', { username, password })).data;
+    return { ...res, auth: true };
+  } catch (err) {
+    const status: number = err.response.status;
+    if (status === 401)
+      return {
+        auth: false,
+      };
+    return {
+      auth: false,
+    };
+  }
 };
 
 export { RequestorLogin };
