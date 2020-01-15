dd97c039ef2eba5a682661c0bc3228abfdc118db
yee2542 <yee2542gtl@gmail.com>
2020-01-15 11:01:29 +0700

    add form validator and ux

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { Row, Col, Input } from 'antd';
+import { Row, Col, Input, Form } from 'antd';
 import Button from '../../components/Button';
+
+import { FormComponentProps } from 'antd/lib/form';
 
 import logo from '../../assets/logo.login.svg';
 import styles from './styles.module.css';
 
-export default class LoginPage extends Component {
+class LoginPage extends Component<FormComponentProps, {}> {
+  state = {};
+
+  onValidator = (rule: any, value: string, callback: any) => {
+    if (value === undefined) return callback('โปรดกรอกรหัสนักศึกษาให้ถูกต้อง');
+    if (value.length !== 11) return callback('โปรดกรอกรหัสนักศึกษาให้ถูกต้อง');
+    return callback();
+  };
+
+  onSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('on login');
+    const { validateFields } = this.props.form;
+    return validateFields((err, values: { username: string; password: string }) => {
+      if (!err) {
+        console.log(values);
+      }
+    });
+  };
+
+  onType = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // const value = e.target.value;
+    const key = e.target.id;
+
+    return this.props.form.setFields({
+      [key]: {
+        error: undefined,
+      },
+    });
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <React.Fragment>
         <div style={{ height: '120px' }} />
@@ -19,18 +52,39 @@ export default class LoginPage extends Component {
           </Col>
         </Row>
         <div style={{ height: '80px' }} />
-        <Row type="flex" justify="center">
-          <Col span={18} lg={14} className={styles.input}>
-            <Input placeholder="Username" />
-          </Col>
-          <Col span={18} lg={14} className={styles.input}>
-            <Input placeholder="Password" />
-          </Col>
-          <Col span={18} lg={14}>
-            <Button>Login</Button>
-          </Col>
-        </Row>
+        <Form onSubmit={this.onSubmit}>
+          <Row type="flex" justify="center">
+            <Col span={18} lg={14} className={styles.input}>
+              <Form.Item>
+                {getFieldDecorator('username', {
+                  rules: [
+                    {
+                      required: true,
+                      max: 11,
+                      validator: this.onValidator,
+                    },
+                  ],
+                  validateTrigger: ['onBlur'],
+                })(<Input onChange={this.onType} placeholder="Username" pattern="[0-9]*" type="number" />)}
+              </Form.Item>
+            </Col>
+            <Col span={18} lg={14} className={styles.input}>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: 'โปรดกรอกรหัสผ่าน' }],
+                  validateTrigger: ['onBlur'],
+                })(<Input onChange={this.onType} placeholder="Password" type="password" />)}
+              </Form.Item>
+            </Col>
+            <Col span={18} lg={14}>
+              <Button>Login</Button>
+            </Col>
+          </Row>
+        </Form>
       </React.Fragment>
     );
   }
 }
+
+const wrapped = Form.create()(LoginPage);
+export default wrapped;
