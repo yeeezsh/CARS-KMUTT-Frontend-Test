6332344def3afa9906e6d11fa814251109eb97fe
yee2542 <yee2542gtl@gmail.com>
2020-01-15 10:01:21 +0700

    implement mockup data

--- src/pages/MyReserve/index.tsx
+++ src/pages/MyReserve/index.tsx
@@ -4,6 +4,10 @@ import PageLayout from '../../components/Layout/Page';
 import StateCard from '../../components/StateCard';
 import Outline from '../../components/Outline';
 
+import Reserve from '../../models/reserve/interface';
+
+import { data as Data } from '../../models/reserve/data';
+
 // helpers
 import OutlineType from './helpers/outline.type';
 
@@ -12,20 +16,29 @@ export default class MyReservePage extends Component<
   {
     type: string;
     outline: string;
+    data: Reserve[];
   }
 > {
-  state = { type: '', outline: '' };
+  state = { type: '', outline: '', data: [] };
 
   componentDidMount = () => {
     const { type } = this.props;
     const outline = OutlineType(type);
-    return this.setState({ type, outline });
+    const data = Data();
+    return this.setState({ type, outline, data });
   };
   render() {
-    const { outline } = this.state;
+    console.log('my reserve', this.state.data);
+    const { outline, data } = this.state;
     return (
       <PageLayout titile={'การจองของฉัน'}>
         <Outline>{outline}</Outline>
+        {/* <StateCard /> */}
+        {data &&
+          data.map((e: Reserve, i) => {
+            const { name, reserve } = e;
+            return <StateCard key={i} name={name} reserve={reserve} />;
+          })}
       </PageLayout>
     );
   }
