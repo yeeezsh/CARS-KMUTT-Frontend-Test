4732099d7b7449f64b0798c9d0ab7e21d32250fb
yee2542 <yee2542gtl@gmail.com>
2020-01-22 23:56:53 +0700

    add week parse helper

---
+++ src/pages/Sport/helpers/week.parse.ts
@@ -0,0 +1,23 @@
+export default (str: string): number[] => {
+  let type = 'delim';
+  if (str.includes('-')) type = 'range';
+  const arr: number[] = [];
+  let strSplit;
+  switch (type) {
+    case 'range':
+      strSplit = str.split('-');
+      const start = Number(strSplit[0]);
+      const end = Number(strSplit[1]);
+      let cur: number = start;
+      while (cur < end) {
+        arr.push(cur);
+        cur++;
+      }
+      return arr;
+    case 'delim':
+      strSplit = str.split(',');
+      const filtered = strSplit.filter(e => e !== ',').map(e => Number(e));
+      return filtered;
+  }
+  return [];
+};
