6b5af1369636270a8626c12e55f58ae96299d0be
yee2542 <yee2542gtl@gmail.com>
2020-01-24 03:52:28 +0700

    update webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        pathRewrite: {'^/api' : ''},
+        // pathRewrite: {'^/api' : ''},
         changeOrigin: true,
         secure: false,
       }
