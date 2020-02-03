e33ad91aa5b7b286b68f0f3338eb36db2a287df8
yee2542 <yee2542gtl@gmail.com>
2020-02-04 00:55:30 +0700

    convert to class type

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -1,64 +1,62 @@
-import instance from './axios.interface';
 import User from './interface';
 import i from '../axios.interface';
-
-const SaveCredential = (data: User) => {
-  try {
-    const duplicated = localStorage.getItem('user');
-    if (duplicated) localStorage.removeItem('user');
-    localStorage.setItem('user', JSON.stringify(data));
-    i.addToken(data.Authorization);
-    return true;
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-};
-
-const DeleteCredential = () => {
-  localStorage.removeItem('user');
-  i.removeToken();
-  return;
-};
 
 const loginAdapter = (type: 'staff' | 'requestor', data: { username: string; password: string }) => {
   let url = '/users/auth/requestor';
   if (type === 'staff') url = '/users/auth/staff';
-  return instance.post(url, data);
+  return i.instance.post(url, data);
 };
 
-const RequestorLogin = async (
-  username: string,
-  password: string,
-): Promise<{ access_token?: string; Authorization?: string; auth: boolean }> => {
-  try {
-    const res = (await loginAdapter('requestor', { username, password })).data;
-    SaveCredential(res);
-    return { ...res, auth: true };
-  } catch (err) {
-    const status: number = err.response.status;
-    if (status === 401)
+class UserClass {
+  user: User | undefined;
+  constructor() {
+    const data = localStorage.getItem('user');
+    this.user = JSON.parse(data || '{}');
+  }
+
+  DeleteCredential = () => {
+    localStorage.removeItem('user');
+    return;
+  };
+
+  SaveCredential = (data: User) => {
+    try {
+      const duplicated = localStorage.getItem('user');
+      if (duplicated) localStorage.removeItem('user');
+      localStorage.setItem('user', JSON.stringify(data));
+
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  RequestorLogin = async (username: string, password: string): Promise<{ auth: boolean }> => {
+    try {
+      const res = (await loginAdapter('requestor', { username, password })).data;
+      this.SaveCredential(res);
+      return { auth: true };
+    } catch (err) {
+      const status: number = err.response.status;
+      if (status === 401)
+        return {
+          auth: false,
+        };
       return {
         auth: false,
       };
-    return {
-      auth: false,
-    };
-  }
-};
+    }
+  };
 
-const UserLogout = () => DeleteCredential();
-const GetUser = (): User => {
-  const data = localStorage.getItem('user');
-  if (!data) throw Error('no user data please login');
-  return JSON.parse(data);
-};
+  UserLogout = async () => {
+    this.DeleteCredential();
+    await i.instance.get('/users/auth/logout');
+  };
 
-const GetUserToken = (): User['Authorization'] => {
-  const data = localStorage.getItem('user');
-  if (!data) throw Error('no user data please login');
-  const parse: User = JSON.parse(data);
-  return parse.Authorization;
-};
+  GetUser = (): User | undefined => {
+    return this.user;
+  };
+}
 
-export { RequestorLogin, UserLogout, GetUser, GetUserToken };
+export const u = new UserClass();
