147448d18b5d6eebba41e2bd0780fe5632ba0633
yee2542 <yee2542gtl@gmail.com>
2020-02-04 00:54:24 +0700

    deprecate self sotr token

--- src/models/axios.interface.ts
+++ src/models/axios.interface.ts
@@ -1,38 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
 // const END_POINT = 'http://157.245.147.102:3000/api';
-const END_POINT = 'http://10.26.100.205:3000/api';
+const END_POINT = 'http://localhost:3000/api';
 
 class Instance {
   instance: AxiosInstance;
-  token: string;
   constructor() {
     this.instance = axios.create({
       baseURL: END_POINT,
       timeout: 5000,
+      withCredentials: true,
     });
-    this.token = '';
-  }
-
-  addToken(token: string) {
-    console.log('adding token to adapter', token);
-    this.token = token;
-    this.instance = axios.create({
-      baseURL: END_POINT,
-      timeout: 2000,
-      headers: {
-        Authorization: token,
-      },
-    });
-    return this;
-  }
-
-  removeToken() {
-    this.token = '';
-    this.instance = axios.create({
-      baseURL: END_POINT,
-      timeout: 2000,
-    });
-    return this;
   }
 }
 const i = new Instance();
