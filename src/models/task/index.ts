1d6f02f27dc39111a190ac4721d25a6247e171e1
yee2542 <yee2542gtl@gmail.com>
2020-02-06 00:19:58 +0700

    parsing iso strinf to moment

--- src/models/task/index.ts
+++ src/models/task/index.ts
@@ -1,10 +1,23 @@
 import i from '../axios.interface';
+import { Task } from './task.interface';
+import moment from 'moment';
 
 class TaskClass {
-  async getLastTask() {
+  async getLastTask(): Promise<Task | undefined> {
     try {
       const data = (await i.instance.get('/task/last')).data;
-      return data;
+      if (!data) return undefined;
+
+      return {
+        ...data,
+        reserve:
+          data &&
+          data.reserve.map((e: { start: string; stop: string }) => ({
+            ...e,
+            start: moment(e.start),
+            stop: moment(e.stop),
+          })),
+      };
     } catch (err) {
       console.error(err);
       throw new Error(err);
