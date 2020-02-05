fb50d18e0731c1dc76225a324c78e7c3b6da3be6
yee2542 <yee2542gtl@gmail.com>
2020-02-05 22:20:19 +0700

    convert to stateless

--- src/components/Layout/Home.tsx
+++ src/components/Layout/Home.tsx
@@ -3,77 +3,76 @@ import { Row, Col } from 'antd';
 import moment from 'moment';
 import Loadable from 'react-loadable';
 
+// interfaces
+import { Task } from '../../models/task/task.interface';
+
 import styles from './home.module.css';
+
 const StateCard = Loadable({
   loader: () => import('../StateCard'),
   loading: () => null,
 });
 
-export default class Home extends Component<{}, { day: moment.Moment }> {
-  state = {
-    // day: moment('30/11/20', 'DD-MM-YY'),
-    day: moment(),
-  };
+const Home: React.FunctionComponent<{ lastCard?: Task }> = props => {
+  const day = moment();
+  const dayName = day.format('ddd');
+  const date = day.format('D');
+  const month = day.format('MMMM');
+  const year = day.format('YYYY');
+  return (
+    <React.Fragment>
+      <Row type="flex" justify="center" className={styles.header}>
+        <Col className={styles.title} span={12}>
+          <p className={styles.white}>หน้าแรก</p>
+        </Col>
+      </Row>
 
-  render() {
-    const { day } = this.state;
-    const dayName = day.format('ddd');
-    const date = day.format('D');
-    const month = day.format('MMMM');
-    const year = day.format('YYYY');
-    return (
-      <React.Fragment>
-        <Row type="flex" justify="center" className={styles.header}>
-          <Col className={styles.title} span={12}>
-            <p className={styles.white}>หน้าแรก</p>
-          </Col>
-        </Row>
+      {/* spacing */}
+      <div style={{ height: 325 }}></div>
 
-        {/* spacing */}
-        <div style={{ height: 325 }}></div>
+      {/* bg color */}
+      <div
+        style={{
+          backgroundColor: '#ff682b',
+          position: 'fixed',
+          height: '335px',
+          width: '100%',
+          top: 0,
+        }}
+      ></div>
 
-        {/* bg color */}
-        <div
-          style={{
-            backgroundColor: '#ff682b',
-            position: 'fixed',
-            height: '335px',
-            width: '100%',
-            top: 0,
-          }}
-        ></div>
+      {/* date and time */}
+      <Row className={styles.date}>
+        <div className={styles.textDate}>Today</div>
+        <div className={styles.textDate}>
+          {dayName}, {date} {month} {year}
+        </div>
+      </Row>
 
-        {/* date and time */}
-        <Row className={styles.date}>
-          <div className={styles.textDate}>Today</div>
-          <div className={styles.textDate}>
-            {dayName}, {date} {month} {year}
-          </div>
-        </Row>
+      {/* card */}
+      <Row type="flex" justify="center">
+        <Col span={23}>
+          <StateCard
+            name={'Badminton 1'}
+            reserve={{
+              date: moment(),
+              start: moment(),
+              stop: moment().add(1, 'hour'),
+              state: {
+                type: 'wait',
+                desc: 'รอการยืนยัน',
+              },
+            }}
+          />
+        </Col>
+      </Row>
 
-        {/* card */}
-        <Row type="flex" justify="center">
-          <Col span={23}>
-            <StateCard
-              name={'Badminton 1'}
-              reserve={{
-                date: moment(),
-                start: moment(),
-                stop: moment().add(1, 'hour'),
-                state: {
-                  type: 'wait',
-                  desc: 'รอการยืนยัน',
-                },
-              }}
-            />
-          </Col>
-        </Row>
+      {/* content */}
+      <Row style={{ marginTop: 20, padding: 0 }}>
+        <Col className={styles.content}>{props.children}</Col>
+      </Row>
+    </React.Fragment>
+  );
+};
 
-        {/* content */}
-        <Row style={{ marginTop: 20, padding: 0 }}>
-          <Col className={styles.content}>{this.props.children}</Col>
-        </Row>
-      </React.Fragment>
-    );
-  }
-}
+export default Home;
