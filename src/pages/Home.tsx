b3e74da5a6578ec391d3e3e3fc7f16a156ce658c
yee2542 <yee2542gtl@gmail.com>
2020-02-06 00:20:09 +0700

    bind data last card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import Loadable from 'react-loadable';
 
 import HomeLayout from '../components/Layout/Home';
+import { task } from '../models/task';
+import { Task } from '../models/task/task.interface';
 
 const HomeMenu = Loadable({
   loader: () => import('../components/KanbanCard'),
   loading: () => null,
 });
 
-export default class Home extends Component {
+export default class Home extends Component<
+  {},
+  {
+    lastCard: Task | undefined;
+  }
+> {
+  state = { lastCard: undefined };
+
+  componentDidMount = async () => {
+    const data = await task.getLastTask();
+
+    return this.setState({ lastCard: data });
+  };
   render() {
+    const { lastCard } = this.state;
+    // console.log(lastCard?.reserve[0].start?.format('HH MM DD'));
     return (
-      <HomeLayout>
+      <HomeLayout lastCard={lastCard}>
         <HomeMenu />
       </HomeLayout>
     );
