7f1c33eb823ee2e167c8f2affe8af070d979c8f8
yee2542 <yee2542gtl@gmail.com>
2020-02-10 22:10:42 +0700

    transform to stateful components

--- src/pages/MyReserve/index.tsx
+++ src/pages/MyReserve/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PageLayout from '../../components/Layout/Page';
 import StateCard from '../../components/StateCard';
@@ -10,23 +10,56 @@ import Reserve from '../../models/reserve/interface';
 
 // helpers
 import OutlineType from './helpers/outline.type';
+import { r } from '../../models/reserve';
 
-const MyReservePage: React.FunctionComponent<{
-  type: 'wait' | 'history' | 'request';
-}> = props => {
-  const { type } = props;
-  const outline = OutlineType(type);
-  const data = Data();
-  return (
-    <PageLayout titile={'การจองของฉัน'}>
-      <Outline>{outline}</Outline>
-      {data &&
-        data.map((e: Reserve, i) => {
-          const { name, reserve } = e;
-          return <StateCard key={i} name={name} reserve={reserve} />;
-        })}
-    </PageLayout>
-  );
-};
+// const MyReservePage: React.FunctionComponent<{
+//   type: 'wait' | 'history' | 'requested';
+// }> = props => {
+//   const { type } = props;
+//   const outline = OutlineType(type);
+//   const data = Data();
+//   return (
+//     <PageLayout titile={'การจองของฉัน'}>
+//       <Outline>{outline}</Outline>
+//       {data &&
+//         data.map((e: Reserve, i) => {
+//           const { name, reserve } = e;
+//           return <StateCard key={i} name={name} reserve={reserve} />;
+//         })}
+//     </PageLayout>
+//   );
+// };
 
-export default MyReservePage;
+export default class MyReservePage extends Component<
+  {
+    type: 'wait' | 'history' | 'requested';
+  },
+  { data?: Reserve[] }
+> {
+  state = {
+    data: [],
+  };
+
+  componentDidMount = async () => {
+    const data = await r.query('history');
+    console.log(data);
+    return this.setState({ data });
+  };
+  render() {
+    const { data } = this.state;
+    const { type } = this.props;
+    const outline = OutlineType(type);
+    return (
+      <PageLayout titile={'การจองของฉัน'}>
+        <Outline>{outline}</Outline>
+        {data &&
+          data.map((e: Reserve, i) => {
+            const { name, reserve } = e;
+            return <StateCard key={i} name={name} reserve={reserve} />;
+          })}
+      </PageLayout>
+    );
+  }
+}
+
+// export default MyReservePage;
