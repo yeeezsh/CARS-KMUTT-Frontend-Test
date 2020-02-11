8700ce8ba58890b93548f4593a3bbe782e7347bf
yee2542 <yee2542gtl@gmail.com>
2020-02-11 14:55:34 +0700

    supported custom color

--- src/components/BreakingLine.tsx
+++ src/components/BreakingLine.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 
-const BreakingLine: React.FunctionComponent<{ lineSize?: number }> = props => {
+const BreakingLine: React.FunctionComponent<{ lineSize?: number; color?: string }> = props => {
+  const { lineSize, color } = props;
   return (
     <Row type="flex" justify="center">
       <Col span={22}>
         <div
           style={{
-            border: `${props.lineSize || 1}px solid #DADADA`,
+            border: `${lineSize || 1}px solid ${color || '#DADADA'}`,
             marginTop: '-4px',
           }}
         />
