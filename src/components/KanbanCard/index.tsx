4ef518cabf80cedfc27003e115030764577bd6a8
yee2542 <yee2542gtl@gmail.com>
2020-02-13 23:12:21 +0700

    add need action mapper via keys

--- src/components/KanbanCard/index.tsx
+++ src/components/KanbanCard/index.tsx
@@ -7,8 +7,22 @@ import { Link } from 'react-router-dom';
 import Menu from '../../models/menu/interface';
 import defaultMenu from '../../models/menu';
 
-export default function KanbanCard(props: { menu?: Menu[] }) {
-  const menu = props.menu ? props.menu : defaultMenu;
+export default function KanbanCard(props: {
+  menu?: Menu[];
+  needActions?: string[];
+}) {
+  const { needActions } = props;
+  let menu = props.menu ? props.menu : defaultMenu;
+
+  // when parse new action
+  if (needActions && needActions[0]) {
+    menu = menu.map(e => {
+      if (needActions.includes(e.key))
+        return { ...e, setting: { ...e.setting, needAction: true } };
+      return e;
+    });
+  }
+
   return (
     <React.Fragment>
       <Row type="flex" justify="space-between">
