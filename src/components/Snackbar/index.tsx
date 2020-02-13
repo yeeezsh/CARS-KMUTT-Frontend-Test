2e64b611b21c5081eefc7526625da1a26e7a83f9
yee2542 <yee2542gtl@gmail.com>
2020-02-13 22:35:34 +0700

    convert to rfc and supported visible props

--- src/components/Snackbar/index.tsx
+++ src/components/Snackbar/index.tsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './style.module.css';
 
-export default class Snackbar extends Component<{ show: boolean }> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      show: true,
-    };
-  }
-  render() {
-    return <div className={styles.snackbar}>{this.props.children}</div>;
-  }
-}
+const Snackbar: React.FunctionComponent<{ show: boolean }> = props => {
+  const { children, show } = props;
+  return (
+    <div
+      className={`${styles.snackbar} ${show ? styles.show : styles.hide}`}
+    >
+      {children}
+    </div>
+  );
+};
+export default Snackbar;
