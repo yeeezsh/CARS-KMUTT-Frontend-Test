2c2ab5cb8e179668444778e6446531101029fe87
yee2542 <yee2542gtl@gmail.com>
2020-02-15 21:39:57 +0700

    preparing sidebar components

---
+++ src/components/Sidebar/icon.import.tsx
@@ -0,0 +1,7 @@
+import docsIcon from 'Assets/icons/drawer/docs.svg';
+import allDocs from 'Assets/icons/drawer/alldocs.svg';
+import rejectDocs from 'Assets/icons/drawer/rejectdocs.svg';
+import acceptDocs from 'Assets/icons/drawer/acceptdocs.svg';
+import dropDocs from 'Assets/icons/drawer/dropdocs.svg';
+
+export { docsIcon, allDocs, rejectDocs, acceptDocs, dropDocs };
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Drawer } from '../../models/drawer/interface';
+
+import styles from './styles.module.css';
+import {} from './icon.import';
+
+// const menu: Drawer[] = [
+//   {
+//     key: '4',
+//     label: ['การจองของฉัน'],
+//     icon: ticketIcon,
+//     sub: [
+//       {
+//         key: '1',
+//         label: ['กำลังดำเนินการ'],
+//         icon: docsIcon,
+//         link: '/my/reserve/wait',
+//       },
+//       {
+//         key: '2',
+//         label: ['รีเควสที่ต้องยืนยัน'],
+//         icon: checkedIcon,
+//         link: '/my/reserve/requested',
+//       },
+//       {
+//         key: '3',
+//         label: ['ประวัติการจอง'],
+//         icon: timeIcon,
+//         link: '/my/reserve/history',
+//       },
+//     ],
+//   },
+// ];
+
+function Sidebar() {
+  return <div></div>;
+}
+
+export default Sidebar;
