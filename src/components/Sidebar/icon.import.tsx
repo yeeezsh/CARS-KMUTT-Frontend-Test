2ec6e5cf94f98ddfa0423bc48e124b384e9d53df
yee2542 <yee2542gtl@gmail.com>
2020-02-15 23:18:25 +0700

    add logo for sidebar

--- src/components/Sidebar/icon.import.tsx
+++ src/components/Sidebar/icon.import.tsx
@@ -3,5 +3,13 @@ import allDocs from 'Assets/icons/drawer/alldocs.svg';
 import rejectDocs from 'Assets/icons/drawer/rejectdocs.svg';
 import acceptDocs from 'Assets/icons/drawer/acceptdocs.svg';
 import dropDocs from 'Assets/icons/drawer/dropdocs.svg';
+import sidebarIcon from 'Assets/logo.sidebar.svg';
 
-export { docsIcon, allDocs, rejectDocs, acceptDocs, dropDocs };
+export {
+  docsIcon,
+  allDocs,
+  rejectDocs,
+  acceptDocs,
+  dropDocs,
+  sidebarIcon,
+};
