f971935c80bb7e787e91c7a4adb07d0eca1bc25d
yee2542 <yee2542gtl@gmail.com>
2020-02-16 19:39:09 +0700

    simple binding data

--- src/pages/Staff/Home.tsx
+++ src/pages/Staff/Home.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import StaffLayout from '../../components/Layout/Staff/Home';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import StaffLayout from 'Components/Layout/Staff/Home';
+import TaskTable from 'Components/TaskTable';
+import { taskTable } from 'Models/taskTable';
+
+import allDocsIcon from 'Assets/icons/staff/alldocs.svg';
 
 function StaffHome() {
-  return <StaffLayout>home staff ja</StaffLayout>;
+  const now = moment().startOf('day');
+  const pagination = moment(now).add(1, 'day');
+  const [data, setData] = useState([]);
+
+  // fetching
+  useEffect(() => {
+    taskTable.getAllTask(now, pagination).then(e => setData(e));
+  }, []);
+
+  return (
+    <StaffLayout>
+      home staff ja
+      <TaskTable title="รายการทั้งหมด" icon={allDocsIcon} data={data} />
+    </StaffLayout>
+  );
 }
 
 export default StaffHome;
