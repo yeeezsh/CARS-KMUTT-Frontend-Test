69a5fc229c814b6a3531d113274a9a2526604eca
yee2542 <yee2542gtl@gmail.com>
2020-02-17 10:27:52 +0700

    add lists state

--- src/components/TaskTable/list.tsx
+++ src/components/TaskTable/list.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import { data } from 'Models/reserve/data';
+
 import { TaskTable } from 'Models/taskTable/interface';
 import moment from 'moment';
+import State from './state';
 import typeDescHelper from './type.desc.helper';
 
 const ListTable: React.FC<{
@@ -37,9 +38,9 @@ const ListTable: React.FC<{
       >
         <Col span={4}>วันที่</Col>
         <Col span={4}>รหัสการจอง</Col>
-        <Col span={4}>ประเภทการจอง</Col>
+        <Col span={3}>ประเภทการจอง</Col>
         <Col span={6}>สถานที่</Col>
-        <Col span={4}>รหัสผู้จอง</Col>
+        <Col span={5}>รหัสผู้จอง</Col>
         <Col span={2}>สถานะ</Col>
       </Row>
     );
@@ -63,10 +64,10 @@ const ListTable: React.FC<{
       <Col span={4}>
         {String(data?._id).slice(0, 3) + '.' + String(data?._id).slice(-4)}
       </Col>
-      <Col span={4}>{typeDescHelper(data?.type)}</Col>
+      <Col span={3}>{typeDescHelper(data?.type)}</Col>
       <Col span={6}>{data?.area.label || data?.area.name}</Col>
-      <Col span={4}>{data?.requestor[0].username}</Col>
-      <Col span={2}>{data?.state.slice(-1)}</Col>
+      <Col span={5}>{data?.requestor[0].username}</Col>
+      <Col span={2}>{<State state={data?.state.slice(-1)[0]} />}</Col>
     </Row>
   );
 };
---
+++ src/components/TaskTable/state.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const State: React.FC<{ state?: string }> = props => {
+  const mainStyle: React.CSSProperties = {
+    fontWeight: 'bold',
+  };
+  const { state } = props;
+  switch (state) {
+    case 'accept':
+      return <p style={{ ...mainStyle, color: '#52C41A' }}>อนุมัติ</p>;
+    case 'reject':
+      return <p style={{ ...mainStyle, color: '#F5222D' }}>ไม่อนุมัติ</p>;
+    case 'drop':
+      return <p style={{ ...mainStyle, color: '#F5222D' }}>ไม่อนุมัติ</p>;
+    case 'wait':
+      return (
+        <p style={{ ...mainStyle, color: '#1890FF' }}>รอการอนุมัติ</p>
+      );
+    case 'requested':
+      return (
+        <p style={{ ...mainStyle, color: '#1890FF' }}>
+          รอการอนุมัติจากเพื่อน
+        </p>
+      );
+  }
+  return <div></div>;
+};
+
+export default State;
