ff917a2c9f5a98d2e88f7e4e4ce45c96da1ffc6a
yee2542 <yee2542gtl@gmail.com>
2020-02-17 10:42:30 +0700

    add all methods data

--- src/models/taskTable/index.ts
+++ src/models/taskTable/index.ts
@@ -1,15 +1,33 @@
 import { Moment } from 'moment';
 import i from 'Models/axios.interface';
+import { TaskTableType } from './interface';
 
 class TaskTable {
-  async getAllTask(offset?: Moment, limit?: Moment) {
-    const res = await i.instance.get('/task/manage/all', {
+  async getTask(
+    offset?: Moment,
+    limit?: Moment,
+    taskType?: 'all' | 'wait' | 'reject' | 'accept',
+  ): Promise<TaskTableType> {
+    const res = await i.instance.get(`/task/manage/${taskType}`, {
       params: {
         offset: offset?.toISOString(),
         limit: limit?.toISOString(),
       },
     });
     return res.data;
+  }
+
+  async getAllTask(offset?: Moment, limit?: Moment) {
+    return this.getTask(offset, limit, 'all');
+  }
+  async getRejectTask(offset?: Moment, limit?: Moment) {
+    return this.getTask(offset, limit, 'reject');
+  }
+  async getAcceptTask(offset?: Moment, limit?: Moment) {
+    return this.getTask(offset, limit, 'accept');
+  }
+  async getWaitTask(offset?: Moment, limit?: Moment) {
+    return this.getTask(offset, limit, 'wait');
   }
 }
 
