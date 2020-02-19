68f8d82595e32e9c03b997c51e3c60fc063e6385
yee2542 <yee2542gtl@gmail.com>
2020-02-19 17:14:56 +0700

    optimize dev env

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,9 +29,11 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
         },
       },
-
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
