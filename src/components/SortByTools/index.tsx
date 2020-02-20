f167c8fb2b0404b83141a99bfca372cf6ea6031c
yee2542 <yee2542gtl@gmail.com>
2020-02-20 17:25:27 +0700

    add allow clear and more mapper field

--- src/components/SortByTools/index.tsx
+++ src/components/SortByTools/index.tsx
@@ -4,7 +4,13 @@ import styles from './styles.module.css';
 
 const { Option } = Select;
 
-type OnSelectedType = 'createAt' | '_id' | 'type' | 'area' | 'state' | any;
+export type OnSelectedType =
+  | 'createAt'
+  | '_id'
+  | 'type'
+  | 'area'
+  | 'state'
+  | any;
 
 const SortByTools: React.FunctionComponent<{
   onSelected?: (s: OnSelectedType) => void;
@@ -17,11 +23,13 @@ const SortByTools: React.FunctionComponent<{
       onSelect={e => props.onSelected && props.onSelected(e)}
       dropdownClassName={styles.dropdown}
       dropdownMenuStyle={{ borderRadius: '25px' }}
+      allowClear={true}
     >
       <Option value="createAt">วันที่</Option>
       <Option value="_id">รหัสการจอง</Option>
       <Option value="type">ประเภทการจอง</Option>
       <Option value="area">สถานที่</Option>
+      <Option value="requestor">รหัสผู้จอง</Option>
       <Option value="state">สถานะ</Option>
     </Select>
   );
