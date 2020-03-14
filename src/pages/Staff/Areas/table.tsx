03d4c5d8ec9194a3fdff38116f6be60d1161feb6
yee2542 <yee2542gtl@gmail.com>
2020-03-14 18:40:11 +0700

    Add history for push to area pages

--- src/pages/Staff/Areas/table.tsx
+++ src/pages/Staff/Areas/table.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable react/display-name */
 import React from 'react';
 import { Table as TableAnt } from 'antd';
 import { AreaTableAPI } from 'Models/area/interfaces';
 import { ColumnProps } from 'antd/lib/table';
+import { useHistory } from 'react-router';
 
-const AreaTable: React.FC<{ data: AreaTableAPI[] }> = props => {
+const AreaTable: React.FC<{
+  data: AreaTableAPI[];
+  loading?: boolean;
+}> = props => {
+  const history = useHistory();
   const columns: ColumnProps<AreaTableAPI>[] = [
     {
       title: 'สนาม / ห้อง',
       key: 'label',
       dataIndex: 'label',
+      render: d => <a>{d}</a>,
       sorter: (a, b) => a.label.localeCompare(b.label),
     },
     {
@@ -20,8 +27,9 @@ const AreaTable: React.FC<{ data: AreaTableAPI[] }> = props => {
   ];
   return (
     <TableAnt
+      loading={props.loading || false}
       pagination={false}
-      onRowClick={e => console.log(e._id)}
+      onRowClick={r => history.push('/staff/area/' + r._id)}
       columns={columns}
       dataSource={props.data}
     />
