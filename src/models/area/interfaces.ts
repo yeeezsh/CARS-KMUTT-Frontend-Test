433e415fd34220249c83a340d2326614192da40b
yee2542 <yee2542gtl@gmail.com>
2020-03-15 18:27:14 +0700

    Add AreaAPI

--- src/models/area/interfaces.ts
+++ src/models/area/interfaces.ts
@@ -1,4 +1,15 @@
+import { AreaBuilding } from './area.building.interfaces';
+import { Moment } from 'moment';
+
 export type AreaBuildingType = 'sport' | 'area' | 'meeting';
+export interface AreaReseveAPI {
+  interval: number;
+  max: number;
+  allDay: boolean;
+  start: Moment;
+  stop: Moment;
+  week: string;
+}
 
 export interface AreaTableAPI {
   _id: string;
@@ -10,4 +21,16 @@ export interface AreaTableAPI {
     label: string;
   };
   type: AreaBuildingType;
+}
+
+export interface AreaAPI {
+  _id: string;
+  name: string;
+  label?: string;
+  building?: AreaBuilding;
+  required?: {
+    requestor: number;
+  };
+  forward: number;
+  reserve: AreaReseveAPI[];
 }
