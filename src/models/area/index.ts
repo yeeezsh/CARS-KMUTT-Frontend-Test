91d9bae7329660c3be5ced87155af9db25ef5ee2
yee2542 <yee2542gtl@gmail.com>
2020-03-15 18:27:02 +0700

    Add some getters API

--- src/models/area/index.ts
+++ src/models/area/index.ts
@@ -1,11 +1,46 @@
 import i from 'Models/axios.interface';
-import { AreaTableAPI } from './interfaces';
+import { AreaTableAPI, AreaAPI as AreaAPIInterfaces } from './interfaces';
+import { AreaAvailableAPI } from './area.interface';
+import moment from 'moment';
 
 class AreaAPI {
   async getBuildingTable(): Promise<AreaTableAPI[]> {
     try {
       const data = (await i.instance.get('/area/table')).data;
       return data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAreaAvailable(id: string): Promise<AreaAvailableAPI[]> {
+    try {
+      const data = (await i.instance.get('/area/available/' + id)).data;
+      console.log('data', data, id);
+      return data.map((e: any) => ({ ...e, date: moment(e.date) }));
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAreaInfo(id: string): Promise<AreaAPIInterfaces> {
+    try {
+      const data: AreaAPIInterfaces = (await i.instance.get('/area/' + id))
+        .data;
+
+      return {
+        ...data,
+        reserve: data.reserve.map(e => ({
+          ...e,
+          start: moment(e.start),
+          stop: moment(e.stop),
+        })),
+      };
+      // return {
+      //   ...data,
+      //   createAt: moment(data.createAt),
+      //   updateAt: moment(data.updateAt),
+      // };
     } catch (err) {
       throw err;
     }
