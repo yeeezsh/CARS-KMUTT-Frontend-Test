d5daad60c542fa6fbd4e31c62f4816358355f2be
yee2542 <yee2542gtl@gmail.com>
2020-03-25 01:29:33 +0700

    Preparing form

--- src/pages/Areas/Common/Activity/index.tsx
+++ src/pages/Areas/Common/Activity/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'antd';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, useHistory, useLocation } from 'react-router';
+import { useSelector, useDispatch } from 'react-redux';
 
 import PageLayout from 'Components/Layout/Page';
 
@@ -14,11 +15,34 @@ import Badge from 'Components/Badge';
 import { RequestorForm } from '../common/forms';
 import BackCard from 'Components/BackCard';
 import Outline from 'Components/Outline';
+import { RootReducers } from 'Store/reducers';
+
+// store
+// import areaFormStore from 'Store/areaForm';
+// import { useDispatch, useStore, Provider } from 'react-redux';
 
 const Activity: React.FC = () => {
-  const [steps, setSteps] = useState(0);
+  const forms = useSelector((s: RootReducers) => s.AreaFormReducers);
+  const steps = forms.step;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation().pathname;
+
+  useEffect(() => {
+    dispatch({ type: 'INIT_FORM', payload: { size: 4 } });
+  }, []);
+
+  // when steps change
+  useEffect(() => {
+    if (steps === 0) return;
+    const oldPath = location;
+    const newPath = oldPath.slice(0, -1) + (steps + 1);
+    history.push(newPath);
+  }, [steps]);
 
   // const [canNext, setCanNext] = useState(false);
+  console.log('forms', forms);
+
   return (
     <PageLayout titile="จองพื้นที่ส่วนกลาง">
       {/* Fixed header */}
--- src/pages/Areas/Common/common/forms/requestor.tsx
+++ src/pages/Areas/Common/common/forms/requestor.tsx
@@ -1,8 +1,78 @@
-import React from 'react';
-import { Form } from 'antd';
+import React, { useEffect } from 'react';
+import { Form, Radio, Row, Col } from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
+import Button from 'Components/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootReducers } from 'Store/reducers';
 
-const RequestorForm = () => {
-  return <Form.Item>test</Form.Item>;
+// constant
+const CUR_IND = 0;
+const RequestorForm: React.FC<FormComponentProps & {
+  ind?: number;
+}> = props => {
+  const { getFieldDecorator, validateFields } = props.form;
+  const dispatch = useDispatch();
+  const canNext = useSelector(
+    (s: RootReducers) => s.AreaFormReducers.canNext,
+  );
+
+  //   set index when form is loaded
+  useEffect(() => {
+    dispatch({
+      type: 'SET_FORM_CUR',
+      payload: { cur: props.ind || CUR_IND },
+    });
+  }, []);
+
+  function onSubmit() {
+    validateFields((err, values) => {
+      dispatch({
+        type: 'FILL_FORM',
+        payload: {
+          form: values,
+          valid: false,
+        },
+      });
+      if (!err) {
+        dispatch({
+          type: 'FILL_FORM',
+          payload: {
+            form: values,
+            valid: true,
+          },
+        });
+        dispatch({ type: 'SUBMIT_FORM' });
+      }
+    });
+  }
+
+  //   function onForm(value)
+
+  return (
+    <React.Fragment>
+      <Form.Item>
+        {getFieldDecorator('userType', {
+          rules: [{ required: true }],
+        })(
+          <Row justify="space-around" type="flex">
+            <Radio.Group>
+              <Radio value="prof">อาจารย์</Radio>
+              <Radio style={{ paddingLeft: 50 }} value="student">
+                นักศึกษา
+              </Radio>
+            </Radio.Group>
+          </Row>,
+        )}
+      </Form.Item>
+      <Col span={24}>
+        <Row type="flex" justify="center">
+          <Col span={22}>
+            <Button onClick={onSubmit}>ต่อไป</Button>
+          </Col>
+        </Row>
+      </Col>
+    </React.Fragment>
+  );
 };
 
 export default Form.create({ name: 'requestor' })(RequestorForm);
