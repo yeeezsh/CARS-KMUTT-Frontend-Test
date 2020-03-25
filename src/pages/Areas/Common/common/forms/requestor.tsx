3d27661cb98faf2ef6a797c42865f2da8a8c482d
yee2542 <yee2542gtl@gmail.com>
2020-03-25 19:20:24 +0700

    Complete first form steps

--- src/pages/Areas/Common/common/forms/requestor.tsx
+++ src/pages/Areas/Common/common/forms/requestor.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
-import { Form, Radio, Row, Col } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Form, Radio, Row, Col, Input, Select } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
 import Button from 'Components/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootReducers } from 'Store/reducers';
+import labelStyles from './styles/label';
+import { faculties, Department } from 'Models/forms/department';
+import { DEFAULT_REQUIRED_RULES } from './rules/required';
 
 // constant
 const CUR_IND = 0;
@@ -12,9 +15,9 @@ const RequestorForm: React.FC<FormComponentProps & {
 }> = props => {
   const { getFieldDecorator, validateFields } = props.form;
   const dispatch = useDispatch();
-  const canNext = useSelector(
-    (s: RootReducers) => s.AreaFormReducers.canNext,
-  );
+  // const canNext = useSelector(
+  //   (s: RootReducers) => s.AreaFormReducers.canNext,
+  // );
 
   //   set index when form is loaded
   useEffect(() => {
@@ -46,13 +49,19 @@ const RequestorForm: React.FC<FormComponentProps & {
     });
   }
 
-  //   function onForm(value)
+  const [departments, setDepartment] = useState<Department>([]);
+  function onFaculty(value: string) {
+    console.log('on faculty', value);
+    const selectedDepartment = faculties.find(e => e.value === value);
+    setDepartment(selectedDepartment?.departments || []);
+  }
 
   return (
     <React.Fragment>
+      {/* user type */}
       <Form.Item>
         {getFieldDecorator('userType', {
-          rules: [{ required: true }],
+          rules: [DEFAULT_REQUIRED_RULES],
         })(
           <Row justify="space-around" type="flex">
             <Radio.Group>
@@ -64,6 +73,76 @@ const RequestorForm: React.FC<FormComponentProps & {
           </Row>,
         )}
       </Form.Item>
+
+      {/* requestor id */}
+      <Form.Item>
+        <span style={labelStyles}>รหัสนักศึกษา</span>
+        {getFieldDecorator('requestorId', {
+          rules: [DEFAULT_REQUIRED_RULES],
+        })(<Input placeholder="รหัสนักศึกษา 11 หลัก" />)}
+      </Form.Item>
+
+      {/* faculties */}
+      <Form.Item>
+        <span style={labelStyles}>คณะ</span>
+        {getFieldDecorator('faculty', {
+          rules: [DEFAULT_REQUIRED_RULES],
+        })(
+          <Select
+            onSelect={onFaculty}
+            style={{ width: '100%', margin: 0, padding: 0 }}
+          >
+            {faculties.map((e, i) => (
+              <Select.Option key={i} value={e.value}>
+                {e.label}
+              </Select.Option>
+            ))}
+          </Select>,
+        )}
+      </Form.Item>
+
+      {/* departments */}
+      <Form.Item>
+        <span style={labelStyles}>ภาควิชา</span>
+        {getFieldDecorator('department', {
+          rules: [DEFAULT_REQUIRED_RULES],
+        })(
+          <Select
+            disabled={!departments[0] ? true : false}
+            style={{ width: '100%', margin: 0, padding: 0 }}
+          >
+            {departments[0] &&
+              departments.map((e, i) => (
+                <Select.Option key={i} value={e.value}>
+                  {e.label}
+                </Select.Option>
+              ))}
+          </Select>,
+        )}
+      </Form.Item>
+
+      {/* Class an d Phone */}
+      <Row type="flex" justify="center" gutter={16}>
+        <Col span={8}>
+          <Form.Item>
+            <span style={labelStyles}>ชั้นปี</span>
+            {getFieldDecorator('year', {
+              rules: [DEFAULT_REQUIRED_RULES],
+              initialValue: 1,
+            })(<Input />)}
+          </Form.Item>
+        </Col>
+        <Col span={16}>
+          <Form.Item>
+            <span style={labelStyles}>โทรศัพท์</span>
+            {getFieldDecorator('phone', {
+              rules: [DEFAULT_REQUIRED_RULES],
+            })(<Input placeholder="091-234-5678" />)}
+          </Form.Item>
+        </Col>
+      </Row>
+
+      {/* action */}
       <Col span={24}>
         <Row type="flex" justify="center">
           <Col span={22}>
---
+++ src/pages/Areas/Common/common/forms/rules/required.ts
@@ -0,0 +1,6 @@
+import { ValidationRule } from 'antd/lib/form';
+
+export const DEFAULT_REQUIRED_RULES: ValidationRule = {
+  required: true,
+  message: 'กรุณากรอก',
+};
