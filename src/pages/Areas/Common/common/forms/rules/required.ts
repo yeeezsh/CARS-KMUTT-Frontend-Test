946323c6979fad38c8c4d9f0bb078798eb252383
yee2542 <yee2542gtl@gmail.com>
2020-04-19 20:43:58 +0700

    add utility sets

--- src/pages/Areas/Common/common/forms/rules/required.ts
+++ src/pages/Areas/Common/common/forms/rules/required.ts
@@ -1,6 +1,8 @@
 import { ValidationRule } from 'antd/lib/form';
 
+export const DEFAULT_REQUIED_MSG = 'กรุณากรอก';
+
 export const DEFAULT_REQUIRED_RULES: ValidationRule = {
   required: true,
-  message: 'กรุณากรอก',
+  message: DEFAULT_REQUIED_MSG,
 };
---
+++ src/pages/Areas/Common/common/forms/styles/font.orange.bold.tsx
@@ -0,0 +1,9 @@
+const fontOrangeBold: React.CSSProperties = {
+  color: '#FF682B',
+  fontWeight: 'bold',
+  margin: 0,
+  padding: 0,
+  marginBottom: -8,
+};
+
+export default fontOrangeBold;
