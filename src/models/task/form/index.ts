7b8e18f095ac75c58f0e18fe2a4d501ccfc23f82
yee2542 <yee2542gtl@gmail.com>
2020-04-22 13:50:46 +0700

    Change Endpoint for task common

--- src/models/task/form/index.ts
+++ src/models/task/form/index.ts
@@ -3,7 +3,7 @@ import i from 'Models/axios.interface';
 class TaskFormClassAPI {
   async createTask(data: any): Promise<void> {
     try {
-      await i.instance.post('/taskForm/', { ...data });
+      await i.instance.post('/taskForm/common', { ...data });
       return;
     } catch (err) {
       console.error(err);
