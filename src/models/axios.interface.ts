664d550498ced67a1188251213818b62fbe7b72f
yee2542 <yee2542gtl@gmail.com>
2020-05-07 21:03:06 +0700

    Add node env automate

--- src/models/axios.interface.ts
+++ src/models/axios.interface.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 //export const END_POINT = 'http://157.245.147.102:3000/api';
-// export const END_POINT = 'http://localhost:3000/api';
-export const END_POINT = 'http://10.26.100.205:3000/api';
+const DEV_END_POINT = 'http://localhost:3000/api';
+const PROD_END_POINT = 'http://10.26.100.205:3000/api';
+
+const mode = process.env.NODE_ENV;
+export const END_POINT =
+  mode === 'development' ? DEV_END_POINT : PROD_END_POINT;
 
 class Instance {
   instance: AxiosInstance;
