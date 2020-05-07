0c78aafddd57e0718d9de61394ef79c6627a7fda
yee2542 <yee2542gtl@gmail.com>
2020-05-07 22:31:09 +0700

    Addd windows hooks

---
+++ src/hooks/windows.resize.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+function useWindowResize() {
+  const [width, setWidth] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleWidth = () =>
+      setWidth({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    window.addEventListener('resize', handleWidth);
+
+    return () => {
+      window.removeEventListener('resize', handleWidth);
+    };
+  });
+
+  return width;
+}
+
+export default useWindowResize;
