338b941be112a8775b17a4be571e1db494c357b3
yee2542 <yee2542gtl@gmail.com>
2020-06-23 17:16:04 +0700

    Add confirm modal type

--- src/components/ConfirmModal/interface.ts
+++ src/components/ConfirmModal/interface.ts
@@ -1,1 +1,11 @@
-export type ConfirmMoalType = 'accept' | 'reject' | 'drop';
+export type ConfirmMoalType = 'accept' | 'reject' | 'drop' | 'forward';
+export interface ConfirmModalTemplate {
+  type: ConfirmMoalType;
+  icon?: string;
+  header: string;
+  desc: string;
+  btn: {
+    bg: string;
+    text: string;
+  };
+}
