232a9fc62f7fd773397b41f28127d99192721ab5
yee2542 <yee2542gtl@gmail.com>
2020-08-03 21:54:55 +0700

    Add forward page

---
+++ src/pages/Staff/Forward.tsx
@@ -0,0 +1,82 @@
+// assets
+import forwardDocsIcon from 'Assets/icons/staff/forwarddocs.svg';
+// data & store
+import { taskTable } from 'Models/taskTable';
+import { TaskTableTypeAPI } from 'Models/taskTable/interface';
+import queryString from 'query-string';
+import React, { useEffect, useState } from 'react';
+import Loadable from 'react-loadable';
+import { useHistory, useLocation } from 'react-router';
+
+const StaffLayout = Loadable({
+  loader: () => import('Components/Layout/Staff/Home'),
+  loading: () => null,
+});
+const TaskTable = Loadable({
+  loader: () => import('Components/TaskTable'),
+  loading: () => null,
+});
+
+const LIMIT = 10;
+const DEFAULT_ORDER_COL = 'createAt';
+
+function StaffForward() {
+  const history = useHistory();
+  const loaction = useLocation();
+  const [data, setData] = useState<TaskTableTypeAPI>({
+    data: [],
+    count: 0,
+  });
+  const [current, setCurrent] = useState(1);
+  const [size, setSize] = useState(LIMIT);
+  const [orderCol, setOrderCol] = useState<string>(DEFAULT_ORDER_COL);
+  const [order, setOrder] = useState<undefined | 1 | -1>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  function setQueryString() {
+    history.replace(
+      `/staff/forward?current=${current ||
+        1}&size=${size}&orderlCol=${orderCol ||
+        DEFAULT_ORDER_COL}&order=${order || '-1'}`,
+    );
+  }
+  // fetching
+  useEffect(() => {
+    setQueryString();
+    setLoading(true);
+    taskTable.getAcceptTask(current, size, orderCol, order).then(e => {
+      setData(e);
+      setLoading(false);
+    });
+  }, [current, size, orderCol, order]);
+
+  // once load
+  useEffect(() => {
+    setQueryString();
+    const query = queryString.parse(loaction.search);
+    setCurrent(Number(query.current || 1));
+    setSize(Number(query.size || LIMIT));
+    setOrderCol(String(query.orderlCol || DEFAULT_ORDER_COL));
+    setOrder(Number(query.order) as 1 | -1);
+  }, []);
+  return (
+    <StaffLayout>
+      <TaskTable
+        title="ส่งต่อ"
+        icon={forwardDocsIcon}
+        data={data.data}
+        loading={loading}
+        current={current}
+        allDataCount={data.count}
+        dataRequest={(pagination, order) => {
+          setCurrent(pagination.current);
+          setSize(pagination.pageSize);
+          setOrderCol(order.column);
+          setOrder(order.order);
+        }}
+      />
+    </StaffLayout>
+  );
+}
+
+export default StaffForward;
