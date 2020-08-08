3c132bda9b1673e7bcebee7a301f2a9136774dee
yee2542 <yee2542gtl@gmail.com>
2020-08-08 23:22:54 +0700

    Fix folder router name

---
+++ src/router/index.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Loadable from 'react-loadable';
+import { Route, Router, Switch } from 'react-router';
+import history from './history';
+
+const PageUserRouter = Loadable({
+  loader: () => import('./user'),
+  loading: () => null,
+});
+
+const PageStaffRouter = Loadable({
+  loader: () => import('./Staff'),
+  loading: () => null,
+});
+
+export default class RootRouter extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          {/* staff zone */}
+          <Route path="/staff">
+            <PageStaffRouter />
+          </Route>
+
+          {/* user zone */}
+          <Route path="/">
+            <PageUserRouter />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
