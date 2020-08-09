520348e1101cf1d48fa8e701940f5a95bb5c56b7
yee2542 <yee2542gtl@gmail.com>
2020-08-09 21:46:08 +0700

    Remove traling comma

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       Pages: path.resolve(__dirname, 'src/pages'),
       Store: path.resolve(__dirname, 'src/store'),
       Utils: path.resolve(__dirname, 'src/utils'),
-      Hooks: path.resolve(__dirname, 'src/hooks'),
+      Hooks: path.resolve(__dirname, 'src/hooks')
     },
   },
   module: {
