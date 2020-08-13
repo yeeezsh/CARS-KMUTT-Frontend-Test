b014a1fafdde10876d6f3b9b94144177035a469c
yee2542 <yee2542gtl@gmail.com>
2020-08-14 00:25:26 +0700

    [WIP] MyReservation factory for form parsing

--- src/pages/MyReserve/MyReservationEdit.tsx
+++ src/pages/MyReserve/MyReservationEdit.tsx
@@ -1,7 +1,22 @@
 import ReservationInfoEdit from 'Components/ReservationInfo/ReservationInfoEdit';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { taskAPI } from 'Services/task';
+import { Task } from 'Services/task/task.interface';
+
+const FormFactory = (type: Task['type']) => {
+  return;
+};
 
 const MyReservationEdit: React.FC = props => {
+  const [task, setTask] = useState<Task>();
+  const { id: taskId } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    taskAPI.getTaskById(taskId).then(t => setTask(t));
+  }, []);
+
+  console.log(taskId, task);
   return (
     <div>
       <ReservationInfoEdit />
