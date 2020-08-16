ad0653a5e3a9e92ddd2af1af6e3e3002ead2caf9
yee2542 <yee2542gtl@gmail.com>
2020-08-16 19:11:25 +0700

    Refactor

--- src/pages/Staff/Task/useCanReject.ts
+++ src/pages/Staff/Task/useCanReject.ts
@@ -1,4 +1,8 @@
-import { TaskDetail, TaskType } from 'Services/task/task.interface';
+import {
+  TaskDetail,
+  TaskStateType,
+  TaskType,
+} from 'Services/task/task.interface';
 
 function useCanReject(task: TaskDetail): boolean {
   // when empty
@@ -7,12 +11,11 @@ function useCanReject(task: TaskDetail): boolean {
   const allowType: TaskType[] = [TaskType.common, TaskType.commonSport];
   if (task.type && !allowType.includes(task.type)) return false;
 
-  const alreadyRejected = task.state.slice(-1)[0] === 'reject';
+  const alreadyRejected = task.state.slice(-1)[0] === TaskStateType.reject;
   if (alreadyRejected) return false;
 
-  if (task.type === 'sport') return false;
   // already drop
-  if (task.state.slice(-1)[0] === 'drop') return false;
+  if (task.state.slice(-1)[0] === TaskStateType.drop) return false;
 
   return true;
 }
