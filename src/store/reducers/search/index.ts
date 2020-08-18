619a93eb207a680a47d4155f6c537f259dc7aa80
Korbboon <yee2542gtl@gmail.com>
2020-08-18 21:56:15 +0700

    Add onQuery reducer action

--- src/store/reducers/search/index.ts
+++ src/store/reducers/search/index.ts
@@ -1,6 +1,6 @@
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
 import { TaskTableTypeAPI } from 'Services/taskTable/interface';
-import { onClear, onSearch } from './actions';
+import { onClear, onQuery, onSearch } from './actions';
 import { OnSearch } from './types';
 export interface SearchState {
   s: string;
@@ -10,17 +10,35 @@ export interface SearchState {
   date?: string;
   data: TaskTableTypeAPI;
   loading: boolean;
+  error: boolean;
 }
 const initState: SearchState = {
   s: '',
   data: { count: 0, data: [] },
   loading: false,
+  error: false,
 };
 
 export const SearchReducers = createReducer(initState, {
-  [onSearch.type]: (state, action: PayloadAction<OnSearch>) => ({
-    ...state,
-    s: action.payload,
-  }),
+  [onSearch.type]: (state, action: PayloadAction<OnSearch>) => {
+    return {
+      ...state,
+      s: action.payload,
+    };
+  },
   [onClear.type]: () => initState,
+  [onQuery.pending.type]: state => ({
+    ...state,
+    loading: true,
+    error: false,
+  }),
+  [onQuery.fulfilled.type]: (
+    state,
+    action: PayloadAction<TaskTableTypeAPI>,
+  ) => ({ ...state, data: action.payload, loading: false, error: false }),
+  [onQuery.rejected.type]: state => ({
+    ...state,
+    loading: false,
+    error: true,
+  }),
 });
