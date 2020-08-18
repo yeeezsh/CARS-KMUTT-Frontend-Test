e0c08e513202a05c92ae75a87dac51466c74d785
Korbboon <yee2542gtl@gmail.com>
2020-08-18 19:04:16 +0700

    Refactor

--- src/store/reducers/search/types.ts
+++ src/store/reducers/search/types.ts
@@ -3,7 +3,5 @@ import { TaskSearchDto } from 'Services/task/task.search.service';
 export const ON_SEARCH = 'SEARCH/ON_SEARCH';
 export const ON_CLEAR = 'SEARCH/ON_CLEAR';
 export const ON_QUERY = 'SEARCH/ON_QUERY';
-export interface OnSearch {
-  s: string;
-}
+export type OnSearch = string;
 export type OnQuery = TaskSearchDto;
