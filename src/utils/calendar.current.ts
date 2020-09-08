9418f5df5d2e7bdd0fce2a70bcc68d7f097edd5d
yee2542 <yee2542gtl@gmail.com>
2020-09-08 21:29:42 +0700

    Add jsdoc

--- src/utils/calendar.current.ts
+++ src/utils/calendar.current.ts
@@ -1,5 +1,7 @@
 import moment, { Moment } from 'moment';
-
+/**
+ * @param  {number} OFFSET_DAY 0 is set for only presentime
+ */
 export default function(OFFSET_DAY: number) {
   return (current: Moment | null): boolean => {
     return Boolean(
