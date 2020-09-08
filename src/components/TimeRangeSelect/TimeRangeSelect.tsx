404ef478fb60189d5ed0ba7d4a076ab8c3291fc1
yee2542 <yee2542gtl@gmail.com>
2020-09-08 21:53:00 +0700

    [WIP] Add time rage selector

--- src/components/TimeRangeSelect/TimeRangeSelect.tsx
+++ src/components/TimeRangeSelect/TimeRangeSelect.tsx
@@ -1,7 +1,35 @@
+import { DatePicker } from 'antd';
+import { RangePickerValue } from 'antd/lib/date-picker/interface';
+import moment, { Moment } from 'moment';
 import React from 'react';
+import calendarCurrent from 'Utils/calendar.current';
 
-const TimeRangeSelect: React.FC = () => {
-  return <div>time range selecting</div>;
+const { RangePicker } = DatePicker;
+const DATEFORMAT = 'DD/MM/YYYY';
+
+const TimeRangeSelect: React.FC<{
+  onSelect: (start: Moment, stop: Moment) => void;
+  forward: number;
+  now: Moment;
+}> = props => {
+  console.log(props);
+  function onChange(dates: RangePickerValue) {
+    const [start, stop] = dates;
+    start && stop && props.onSelect(start, stop);
+  }
+  return (
+    <div>
+      <RangePicker
+        defaultValue={[
+          moment(props.now),
+          moment(props.now).add(props.forward, 'day'),
+        ]}
+        disabledDate={calendarCurrent(0)}
+        format={DATEFORMAT}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default TimeRangeSelect;
