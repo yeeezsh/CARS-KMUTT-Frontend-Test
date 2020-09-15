1b9eced21e93f6c1e728d746bdc4978f10559f5a
yee2542 <yee2542gtl@gmail.com>
2020-09-15 16:37:13 +0700

    Support AreabuildingEnum query

--- src/pages/Staff/Area/hooks/useOnReserveTimeTableAPI.tsx
+++ src/pages/Staff/Area/hooks/useOnReserveTimeTableAPI.tsx
@@ -1,11 +1,52 @@
 import { message } from 'antd';
 import TimeNode from 'Components/TimeTable/timetable.interface';
 import { Moment } from 'moment';
+import AreaBuildingEnum from 'Services/area/@enums/area.building.enum';
 import { AreaAPI } from 'Services/area/interfaces';
 import { taskAPI } from 'Services/task';
+import { taskMeetingAPI } from 'Services/task/meeting';
+import { CreateTaskMeeting } from 'Services/task/meeting/interface';
 import { CreateTaskByStaff } from 'Services/task/task.create.interface';
 import { UserClass } from 'Services/user';
 import SelectedDateType from '../@types/selected.date.type';
+
+function selectingToDateAPI(e: TimeNode[], areaInfo: AreaAPI) {
+  return e.map(t => ({
+    start: t.value.toDate(),
+    stop: t.value.add(areaInfo.reserve[0].interval, 'minutes').toDate(),
+    allDay: false,
+  }));
+}
+
+async function sportTaskAPI(
+  selecting: TimeNode[][],
+  areaInfo: AreaAPI,
+  u: UserClass,
+) {
+  const parser: CreateTaskByStaff = {
+    time: [],
+    area: areaInfo._id,
+    owner: u.GetUser()._id,
+    requestor: [u.GetUser().username],
+  };
+  const mapped = selecting
+    .map(e => ({
+      ...parser,
+      time: selectingToDateAPI(e, areaInfo),
+    }))
+    .filter(e => e.time.length > 0);
+  await Promise.all(mapped.map(e => taskAPI.createSportTaskByStaff(e)));
+}
+
+async function meetingTaskAPI(selecting: TimeNode[][], areaInfo: AreaAPI) {
+  const mapped: CreateTaskMeeting[] = selecting.map(e => ({
+    time: selectingToDateAPI(e, areaInfo),
+    area: areaInfo._id,
+  }));
+  await Promise.all(
+    mapped.map(e => taskMeetingAPI.createMeetingTaskByStaff(e)),
+  );
+}
 
 async function callAPI(
   u: UserClass,
@@ -14,28 +55,19 @@ async function callAPI(
   selecting: TimeNode[][],
   onCancel: () => void,
   fetchData: (startDate: Moment, stopDate: Moment) => void,
+  API: AreaBuildingEnum,
 ) {
-  const parser: CreateTaskByStaff = {
-    time: [],
-    area: areaInfo._id,
-    owner: u.GetUser()._id,
-    requestor: [u.GetUser().username],
-  };
   if (!areaInfo) throw new Error('need area info');
-  const mapped = selecting
-    .map(e => ({
-      ...parser,
-      time: e.map(t => ({
-        start: t.value.toDate(),
-        stop: t.value
-          .add(areaInfo.reserve[0].interval, 'minutes')
-          .toDate(),
-        allDay: false,
-      })),
-    }))
-    .filter(e => e.time.length > 0);
 
-  await Promise.all(mapped.map(e => taskAPI.createSportTaskByStaff(e)));
+  switch (API) {
+    case AreaBuildingEnum.sport:
+      await sportTaskAPI(selecting, areaInfo, u);
+      break;
+    case AreaBuildingEnum.meeting:
+      await meetingTaskAPI(selecting, areaInfo);
+      break;
+  }
+
   // taskMeetingAPI.createMeetingTask({ time: mapped, area: areaInfo._id });
 
   selectedDate && fetchData(selectedDate.start, selectedDate.stop);
@@ -48,11 +80,20 @@ function useOnRserveTimeTableAPI(
   areaInfo: AreaAPI,
   onCancel: () => void,
   fetchData: (startDate: Moment, stopDate: Moment) => void,
+  API: AreaBuildingEnum,
 ): ((selectedDate: SelectedDateType, selecting: TimeNode[][]) => void)[] {
   try {
     return [
       (selectedDate, selecting) => {
-        callAPI(u, areaInfo, selectedDate, selecting, onCancel, fetchData);
+        callAPI(
+          u,
+          areaInfo,
+          selectedDate,
+          selecting,
+          onCancel,
+          fetchData,
+          API,
+        );
       },
     ];
   } catch (err) {
