e76e82e228cfb6ef9bf43772f88fc64f4ef300ca
Korbboon <korbboon.thuswongsa@agoda.com>
2020-09-18 03:42:31 +0700

    Fix UI issues

--- src/components/Layout/Page/index.tsx
+++ src/components/Layout/Page/index.tsx
@@ -1,27 +1,41 @@
 import { Col, Row } from 'antd';
-import React, { Component } from 'react';
+import useWindowResize from 'Hooks/use.windows.resize';
+import React from 'react';
 import styles from './page.module.css';
 
-export default class PageLayout extends Component<{
-  titile: string;
-}> {
-  render() {
-    return (
-      <Row>
-        {/* header */}
-        <Row type="flex" justify="center" className={styles.header}>
-          <Col className={styles.title} span={12}>
-            <p className={styles.orange}>{this.props.titile}</p>
-          </Col>
-        </Row>
+const PageLayout: React.FC<{ titile: string }> = props => {
+  const width = useWindowResize();
+  console.log(width);
+  return (
+    <Row>
+      {/* hotfix background */}
+      <Row
+        style={{
+          backgroundColor: 'white',
+          zIndex: 1,
+          width: width.width,
+          height: '190px',
+          position: 'fixed',
+        }}
+      />
 
-        {/* content */}
-        <Row style={{ marginTop: 75 }} type="flex" justify="center">
-          {React.Children.map(this.props.children, child => (
-            <Col span={22}>{child}</Col>
-          ))}
-        </Row>
+      {/* header */}
+      <Row type="flex" justify="center">
+        <div className={styles.header}>
+          <Col className={styles.title} span={20}>
+            <p className={styles.orange}>{props.titile}</p>
+          </Col>
+        </div>
       </Row>
-    );
-  }
-}
+
+      {/* content */}
+      <Row style={{ marginTop: 75 }} type="flex" justify="center">
+        {React.Children.map(props.children, child => (
+          <Col span={22}>{child}</Col>
+        ))}
+      </Row>
+    </Row>
+  );
+};
+
+export default PageLayout;
