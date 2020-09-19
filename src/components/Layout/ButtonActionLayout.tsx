553adefdd0a21db60c3c5e82d2a125f7e4cd1556
Korbboon <korbboon.thuswongsa@agoda.com>
2020-09-19 10:58:43 +0700

    Implement reducer to components

--- src/components/Layout/ButtonActionLayout.tsx
+++ src/components/Layout/ButtonActionLayout.tsx
@@ -1,9 +1,26 @@
 import { Col } from 'antd';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootReducersType } from 'Store/reducers';
+
+const baseLayout: React.CSSProperties = {
+  bottom: 50,
+  position: 'fixed',
+  width: '90%',
+};
+
+const staffLayout: React.CSSProperties = {
+  bottom: 50,
+  position: 'relative',
+  width: '100%',
+};
 
 const ButtonActionLayout: React.FC = props => {
+  const style = useSelector(
+    (s: RootReducersType) => s.LayoutReducer.buttonActionLayout,
+  );
   return (
-    <Col span={24} style={{ bottom: 50, position: 'fixed', width: '90%' }}>
+    <Col span={24} style={style === 'base' ? baseLayout : staffLayout}>
       {props.children}
     </Col>
   );
