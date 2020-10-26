da80210a372153d6443d104fa0f068ec1dc23a8f
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-26 21:50:54 +0700

    Add share component

---
+++ src/components/Forms/Common/common/CustomBreakLine.tsx
@@ -0,0 +1,8 @@
+import BreakingLine from 'Components/BreakingLine';
+import React from 'react';
+
+const CustomBreakLine: React.FC = () => (
+  <BreakingLine color="#91D5FF" lineSize={0.25} />
+);
+
+export default CustomBreakLine;
---
+++ src/components/Forms/Common/common/CustomParagraph.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CustomParagraph: React.FC = props => (
+  <p
+    style={{
+      color: '#666666',
+      fontSize: '14px',
+      margin: 0,
+      marginLeft: '8px',
+    }}
+  >
+    {props.children}
+  </p>
+);
+
+export default CustomParagraph;
