785e8f2e1460051d96a5efc7a5797bb6e91531f3
yee2542 <yee2542gtl@gmail.com>
2020-12-21 22:21:54 +0700

    remove ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
 import Loadable from 'react-loadable';
 import { u } from 'Services/user';
 import './App.css';
@@ -13,12 +12,6 @@ class App extends Component {
   componentDidMount() {
     try {
       u.RestoreUser();
-      if (process.env.REACT_APP_GA_KEY) {
-        ReactGA.initialize(process.env.REACT_APP_GA_KEY);
-        ReactGA.pageview(
-          window.location.pathname + window.location.search,
-        );
-      }
     } catch (err) {
       console.error(err);
     }
