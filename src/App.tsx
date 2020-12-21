72ca02e927f58e4f21e93aad62a1aa67fba558f0
yee2542 <yee2542gtl@gmail.com>
2020-12-21 21:14:22 +0700

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,12 @@ const RootRouter = Loadable({
   loading: () => null,
 });
 
-// const EXCEPION_PATH = ['/login', '/staff/login', '/staff'];
-
 class App extends Component {
   componentDidMount() {
     try {
-      // get token before initial app
-      // const exceptPath = EXCEPION_PATH.includes(window.location.pathname);
-      // if (exceptPath) return;
       u.RestoreUser();
-      // if (!u.GetUser()._id) throw Error('user need login');
     } catch (err) {
-      // const staffPath = window.location.pathname.includes('staff');
-      // if (staffPath) return window.location.replace('/staff/login');
-      // return window.location.replace('/login');
+      console.error(err);
     }
   }
 
