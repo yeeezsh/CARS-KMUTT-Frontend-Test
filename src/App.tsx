dd772c953c1f403edca1fb10d3e06e31b22c7fd1
yee2542 <yee2542gtl@gmail.com>
2020-12-21 21:56:00 +0700

    Add tracking id

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
 import Loadable from 'react-loadable';
 import { u } from 'Services/user';
 import './App.css';
@@ -11,6 +12,8 @@ const RootRouter = Loadable({
 class App extends Component {
   componentDidMount() {
     try {
+      ReactGA.initialize('G-VSK5G6Y1LF');
+      ReactGA.pageview(window.location.pathname + window.location.search);
       u.RestoreUser();
     } catch (err) {
       console.error(err);
