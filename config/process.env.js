e03e91a99663e91160d1e3a00101ac5587def918
yee2542 <yee2542gtl@gmail.com>
2020-12-21 23:51:30 +0700

    hotfix ga

--- config/process.env.js
+++ config/process.env.js
@@ -2,4 +2,5 @@ module.exports = {
   REACT_APP_BACKEND_ENDPOINT: JSON.stringify(
     process.env.REACT_APP_BACKEND_ENDPOINT,
   ),
+  REACT_APP_GA_KEY: JSON.stringify(process.env.REACT_APP_GA_KEY),
 };
