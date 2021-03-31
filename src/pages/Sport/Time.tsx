923223f524a72370efcb599fcc9005ccd34ceb1e
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-03-31 21:58:00 +0700

    Hotfix sport page (#77)
    * fix bad disable map ui
    
    * preveting from element cache
    
    * remove smell code
    
    * fix bad staff time range
    
    * show selected time on sport form fill

--- src/pages/Sport/Time.tsx
+++ src/pages/Sport/Time.tsx
@@ -176,7 +176,7 @@ class TimePage extends Component<OwnProps & StateProps, any> {
         {areas &&
           areas.map(e => {
             const { area, time } = e;
-            const start = moment(time.start).startOf('hour');
+            const start = moment(time.start);
             const weekParsed = WeekParseHelper(e.time.week);
 
             if (!weekParsed.includes(selectedWeek)) return null;
@@ -190,7 +190,6 @@ class TimePage extends Component<OwnProps & StateProps, any> {
               });
               cur.add(time.interval || 60, 'minute');
             }
-            disabledMapped.push({ value: moment(cur), type: 'available' });
             disabledMapped = disabledMapped
               .map(e => {
                 const valueMapped = moment(
